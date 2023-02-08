e123dd66d321e7cde8c3de3dcdac02c8a532f5ce
devJuliaLeal <juliaassuncaoleal@gmail.com>
2023-02-08 09:04:20 -0300

    import MUI elements

--- landingpage/src/components/Navbar.jsx
+++ landingpage/src/components/Navbar.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
-import Logo from '../Assets/Logo.svg'
+import Logo from '../Assets/Logo.svg';
+import {BsCart2} from 'react-icons/bs'
+import {HiOutlineBar3} from 'react-icons/hi2'
+import {Box, Drawer, ListItem, ListItemButton, ListItemText, } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home'; 
+import InfoIcon from '@mui/icons-material/Info'
+import CommentRoundedIcon  from '@mui/icons-material/CommentRounded';
+import PhoneRoundedIcon  from '@mui/icons-material/PhoneRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+
+
 
 
 const Navbar = () => {
